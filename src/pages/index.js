6076b748574a411f389d03493579f1dd5d4db5dd
Lexie <sb.alexandria@gmail.com>
2018-09-08 08:41:04 +0700

    first-commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello Everyone😃, I am Lexie.</h1>
+    <p>I started learning coding from May 13, going through Free Code Camp certificate. I would like to have
+     the experience and see how Hackathons work. I am absolutely enjoying learning to code and love coding.</p>
+    <p>I am looking forward to meeting other developers and let's build something.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
